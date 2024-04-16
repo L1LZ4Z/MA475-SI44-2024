545edce2a0d87ff86d77b9ee1232bb1be1b267ce
L1LZ4Z <joaquinorozco2004@gmail.com>
2024-04-16 18:41:12 -0500

    Funciones arregladas, estructura general. Falta responsividad

--- js/script.js
+++ js/script.js
@@ -1,4 +1,41 @@
-function expansiónHistograma(datosImagen) {
+let imageVariable;
+const fileInput = document.getElementById('file-input');
+
+fileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        imageVariable = e.target.result;
+        var canvasOriginal = document.getElementById('canvas-original');
+        var contextoOriginal = canvasOriginal.getContext('2d');
+        var canvasProcesado = document.getElementById('canvas-procesado');
+        var contextoProcesado = canvasProcesado.getContext('2d');
+
+        var imagen = new Image();
+        imagen.onload = function() {
+            // Establecer el tamaño del canvas al tamaño de la imagen
+            canvasOriginal.width = imagen.width;
+            canvasOriginal.height = imagen.height;
+            canvasProcesado.width = imagen.width;
+            canvasProcesado.height = imagen.height;
+            
+            // Dibujar la imagen en el canvas
+            contextoOriginal.clearRect(0, 0, canvasOriginal.width, canvasOriginal.height);
+            contextoOriginal.drawImage(imagen, 0, 0);
+            if (imagen.complete && newWidth > 0 && newHeight > 0) {
+                console.log('Imagen cargada correctamente');
+            } else {
+                alert('Error al cargar la imagen');
+            }
+        };
+        imagen.src = imageVariable;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+function expansionHistograma(datosImagen) {
     var píxeles = datosImagen.data;
     var mínimo = 255, máximo = 0;
 
@@ -19,7 +56,7 @@ function expansiónHistograma(datosImagen) {
     return datosImagen;
 }
 
-function ecualizaciónHistograma(datosImagen) {
+function ecualizacionHistograma(datosImagen) {
     var píxeles = datosImagen.data;
     var histograma = new Array(256).fill(0);
     var histogramaAcumulado = new Array(256).fill(0);
@@ -44,36 +81,36 @@ function ecualizaciónHistograma(datosImagen) {
 }
 
 
-function aplicarExpansiónHistograma() {
+function aplicarExpansionHistograma() {
     var canvasOriginal = document.getElementById('canvas-original');
-    var canvasProcesado = document.getElementById('canvas-procesado');
     var contextoOriginal = canvasOriginal.getContext('2d');
+    var canvasProcesado = document.getElementById('canvas-procesado');
     var contextoProcesado = canvasProcesado.getContext('2d');
     
     var imagen = new Image();
     imagen.onload = function() {
-        contextoOriginal.drawImage(imagen, 0, 0, canvasOriginal.width, canvasOriginal.height);
         var datosImagenOriginal = contextoOriginal.getImageData(0, 0, canvasOriginal.width, canvasOriginal.height);
-        var datosImagenExpandida = expansiónHistograma(datosImagenOriginal);
+        var datosImagenExpandida = expansionHistograma(datosImagenOriginal);
         contextoProcesado.putImageData(datosImagenExpandida, 0, 0);
     };
-    imagen.src = 'img/example1.jpg';
+    imagen.src = imageVariable;
 }
 
 
-function aplicarEcualizaciónHistograma() {
-    
+function aplicarEcualizacionHistograma() {
     var canvasOriginal = document.getElementById('canvas-original');
-    var canvasProcesado = document.getElementById('canvas-procesado');
     var contextoOriginal = canvasOriginal.getContext('2d');
+    var canvasProcesado = document.getElementById('canvas-procesado');
     var contextoProcesado = canvasProcesado.getContext('2d');
     
     var imagen = new Image();
     imagen.onload = function() {
-        contextoOriginal.drawImage(imagen, 0, 0, canvasOriginal.width, canvasOriginal.height);
         var datosImagenOriginal = contextoOriginal.getImageData(0, 0, canvasOriginal.width, canvasOriginal.height);
-        var datosImagenEcualizada = ecualizaciónHistograma(datosImagenOriginal);
-        contextoProcesado.putImageData(datosImagenEcualizada, 0, 0);
+        var datosImagenExpandida = ecualizacionHistograma(datosImagenOriginal);
+        contextoProcesado.putImageData(datosImagenExpandida, 0, 0);
     };
-    imagen.src = 'img/example1.jpg';
+    imagen.src = imageVariable;
 }
+
+
+
